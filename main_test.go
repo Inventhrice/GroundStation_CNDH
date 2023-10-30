de413441cafcf968dd2665191d3d3333aa9f0da2
sebastian-j-ibanez <sebas.ibanez219@gmail.com>
2023-10-30 16:14:34 -0400

    Created demo unit test using testify package.

---
+++ main_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func SetupTestServer() *gin.Engine {
+	server := gin.Default()
+	return server
+}
+
+func TestDemoRoute(t *testing.T) {
+	expected := `{"error": "data not found!"}`
+	server := gin.Default()
+	server.GET("/telemetry/0")
+	req, _ := http.NewRequest("GET", "/telemetry/0", nil)
+	w := httptest.NewRecorder()
+	server.ServeHTTP(w, req)
+
+	actual, _ := io.ReadAll(w.Body)
+	assert.Equal(t, expected, string(actual))
+	assert.Equal(t, http.StatusOK, w.Code)
+}
