04749dda1b0b61d3ba29b2095363cdea3f6e5153
sebastian-j-ibanez <sebas.ibanez219@gmail.com>
2023-11-07 14:56:06 -0500

    Finished Test01 and Test02.

--- main_test.go
+++ main_test.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -15,15 +17,50 @@ func SetupTestServer() *gin.Engine {
 	return server
 }
 
-func Test_GetTelemetry(t *testing.T) {
+func Test01_GetTelemetry_NoData(t *testing.T) {
 	expected := `{"error":"data not found!"}`
 	server := gin.Default()
-	server.GET("/telemetry/0", getTelemetry)
-	req, _ := http.NewRequest("GET", "/telemetry/0", nil)
+	server.GET("/telemetry/", getTelemetry)
+	req, _ := http.NewRequest("GET", "/telemetry/?id=0", nil)
 	w := httptest.NewRecorder()
 	server.ServeHTTP(w, req)
 
 	actual, _ := io.ReadAll(w.Body)
 	assert.Equal(t, expected, string(actual))
 	assert.Equal(t, http.StatusNotFound, w.Code)
+}
+
+func Test02_GetTelemetry_WithData(t *testing.T) {
+	// Create mock data
+	coords := Coordinates{X: "1", Y: "1", Z: "1"}
+	rot := Rotations{P: "1", Y: "1", R: "1"}
+	stat := Status{
+		PayloadPower: "",
+		DataWaiting:  "",
+		ChargeStatus: "",
+		Voltage:      "",
+	}
+	data := TelemetryData{Coordinates: coords, Rotations: rot, Status: stat}
+
+	// Set the telemetryDB with mock data
+	var index = "0"
+	telemetryDB[index] = data
+	filename := "test/test2.tmpl"
+	expectedBody, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error opening test html file.")
+	}
+	expectedCode := http.StatusOK
+	server := gin.Default()
+	server.LoadHTMLFiles("UI/index.tmpl")
+	server.GET("/telemetry/", getTelemetry)
+	req, _ := http.NewRequest("GET", "/telemetry/?id=0", nil)
+	w := httptest.NewRecorder()
+	server.ServeHTTP(w, req)
+	actual, _ := io.ReadAll(w.Body)
+    if err := os.WriteFile(filename, []byte(string(actual)), 0666); err != nil {
+     fmt.Println(err)
+    }
+	assert.Equal(t, string(expectedBody), string(actual))
+	assert.Equal(t, expectedCode, w.Code)
 }
