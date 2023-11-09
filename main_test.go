94d3d1787d951b80cf7c2a2845f8acee1dc35680
sebastian-j-ibanez <sebas.ibanez219@gmail.com>
2023-11-09 12:14:20 -0500

    Reimplemented Tests 01, 02, and 03 to match task 493.

--- main_test.go
+++ main_test.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+    "bytes"
+	"encoding/json"
+    "io"
+    "os"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -13,6 +17,82 @@ func SetupTestServer() *gin.Engine {
 	gin.SetMode(gin.ReleaseMode)
 	server := setupServer()
 	return server
+}
+
+func Test01_PutTelemetry_ValidInput(t *testing.T) {
+	// Create input data
+	coords := Coordinates{X: "1", Y: "1", Z: "1"}
+	rot := Rotations{P: "1", Y: "1", R: "1"}
+	stat := Status{}
+	data := TelemetryData{Coordinates: coords, Rotations: rot, Status: stat}
+    // Convert data into JSON encoded byte array
+    jsonData, err := json.Marshal(data)
+    if err != nil {
+        t.Fatal(err)
+    }
+    // Convert JSON encoded byte array into Reader
+    body := bytes.NewReader(jsonData)
+    // Initialize test server
+	server := SetupTestServer()
+    // Make HTTP request
+	req, _ := http.NewRequest("PUT", "/telemetry/", body)
+	w := httptest.NewRecorder()
+	server.ServeHTTP(w, req)
+	actual, _ := io.ReadAll(w.Body)
+    expectedBody := "{\"message\":\"Data saved successfully!\"}" 
+	expectedCode := http.StatusOK
+    // Get actual json data
+    actualFilename := "telemetry.json"
+    actualJson, err := os.ReadFile(actualFilename)
+    if err != nil {
+        t.Fatal(err)
+    }
+    // Get expected json data
+	expectedFilename := "test/test1.json"
+	expectedJson, err := os.ReadFile(expectedFilename)
+	if err != nil {
+        t.Fatal(err)
+	}
+	assert.Equal(t, string(expectedBody), string(actual))
+	assert.Equal(t, expectedCode, w.Code)
+    assert.Equal(t, expectedJson, actualJson)
+}
+
+func Test02_PutTelemetry_InvalidInput(t *testing.T) {
+    // Initialize test server
+    server := SetupTestServer()
+    // Make HTTP request
+    req, _ := http.NewRequest("PUT", "/telemetry/", nil)
+    w := httptest.NewRecorder()
+    server.ServeHTTP(w, req)
+    actual, _ := io.ReadAll(w.Body)
+    // Set response expected code and body
+    expectedCode := http.StatusBadRequest
+    expectedBody := "{\"error\":\"invalid request\"}"
+    assert.Equal(t, expectedBody, string(actual))
+    assert.Equal(t, expectedCode, w.Code)
+}
+
+func Test03_GetTelemetry_Valid(t *testing.T) {
+    // Initialize test server
+    server := SetupTestServer()
+    // Make HTTP request
+    req, _ := http.NewRequest("GET", "/telemetry/", nil)
+    w := httptest.NewRecorder()
+    server.ServeHTTP(w, req)
+    actual, _ := io.ReadAll(w.Body)
+    // Save index.tmpl
+    filename := "test/test3.tmpl"
+    //if err := os.WriteFile(filename, []byte(string(actual)), 0666); err != nil {
+    //    t.Fatal(err)
+    //}
+    expectedBody, err := os.ReadFile(filename)
+    if err != nil {
+        t.Fatal(err)
+    }
+    expectedCode := http.StatusOK
+    assert.Equal(t, string(expectedBody), string(actual))
+    assert.Equal(t, expectedCode, w.Code)
 }
 
 func Test04_Scripts_Valid(t *testing.T) {
@@ -28,12 +108,12 @@ func Test04_Scripts_Valid(t *testing.T) {
 	assert.Equal(t, expected, w.Body.String())
 }
 
-func Test05_Styles_Valid(t *testing.T) {
-	expected := "testTextfor css file"
+func Test04_Scripts_Invalid(t *testing.T) {
+	expected := "{\"error\":\"open ./UI/scripts/NOTFOUND.js: The system cannot find the file specified.\"}"
 
 	server := SetupTestServer()
 
-	req, _ := http.NewRequest("GET", "/styles/test.css", nil)
+	req, _ := http.NewRequest("GET", "/scripts/NOTFOUND.js", nil)
 	w := httptest.NewRecorder()
 
 	server.ServeHTTP(w, req)
@@ -41,12 +121,12 @@ func Test05_Styles_Valid(t *testing.T) {
 	assert.Equal(t, expected, w.Body.String())
 }
 
-func Test04_Scripts_Invalid(t *testing.T) {
-	expected := "{\"error\":\"open ./UI/scripts/NOTFOUND.js: The system cannot find the file specified.\"}"
+func Test05_Styles_Valid(t *testing.T) {
+	expected := "testTextfor css file"
 
 	server := SetupTestServer()
 
-	req, _ := http.NewRequest("GET", "/scripts/NOTFOUND.js", nil)
+	req, _ := http.NewRequest("GET", "/styles/test.css", nil)
 	w := httptest.NewRecorder()
 
 	server.ServeHTTP(w, req)
